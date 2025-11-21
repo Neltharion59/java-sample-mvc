93ee5e59242da937a5325ab4fc030a32532449fd
lradosky <lradosky@gratex.com>
2025-11-21 22:03:33 +0100

    Single examples

---
+++ src/Outlier/FriendlyClass.java
@@ -0,0 +1,10 @@
+package Outlier;
+
+public class FriendlyClass {
+	public void Method01() {}
+	public int Method02() { return 5; }
+	public void Method03() {
+		DistantClass c = new DistantClass();
+		c.MethodX();
+	}
+}
--- src/Outlier/LoneClass.java
+++ src/Outlier/LoneClass.java
@@ -1,5 +1,60 @@
 package Outlier;
 
-public class LoneClass {
+import java.util.Random;
 
+public class LoneClass {
+	// Empty method - no diagram to draw
+	public void A() {
+	}
+	// No method invocation - no diagram to draw
+	public void B() {
+		int i = 5;
+	}
+	// No method invocation - no diagram to draw
+	public void C() {
+		int i = 5;
+		if (i < 7)
+		{
+			i = i + 2;
+		}
+	}
+	// Only invocations of built-in methods - no diagram to draw
+	public void D() {
+		System.out.print("Hello World");
+		Random r = new Random();
+		r.nextBoolean();
+	}
+	// Invoke 1 Method - return void
+	public void E() {
+		FriendlyClass friend = new FriendlyClass();
+		friend.Method01();
+	}
+	// Invoke 1 Method - return type
+	public void F() {
+		FriendlyClass friend = new FriendlyClass();
+		friend.Method02();
+	}
+	// Invoke 2 Methods - fork
+	public void G() {
+		FriendlyClass friend = new FriendlyClass();
+		friend.Method01();
+		friend.Method02();
+	}
+	// Invoke 2 Methods - cascade
+	public void H() {
+		FriendlyClass friend = new FriendlyClass();
+		friend.Method03();
+	}
+	// If - simple condition, two branches
+	public void I() {
+		FriendlyClass friend = new FriendlyClass();
+		
+		int x = 5;
+		if (x > 8) {
+			friend.Method01();
+		}
+		else {
+			friend.Method03();
+		}
+	}
 }
